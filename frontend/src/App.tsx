df83e82de4f06ae38abfbf6ff36665c0709834f9
JohanBirger <johankoddar@gmail.com>
2023-07-17 15:07:12 +0200

    removed minimal example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,20 +19,7 @@ import './config'
 
 
 function App() {
-  useEffect(() => {
-    // set a cookie when the component mounts
-    Cookies.set('myCookie', 'myCookieValue');
-    
-    // send a GET request to our backend using axios, and include cookies with the request
-    axios.post(`${process.env.BACKEND_URL}`, { withCredentials: true })
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-  }, []);
+ 
   initVisitor();
   return (
     <Router>
