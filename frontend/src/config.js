8b851589374f2b2a144e33c7f65c25e3c09d97fb
JohanBirger <67735706+JohanBirger@users.noreply.github.com>
2023-07-22 16:20:15 +0200

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000"; //"https://prickly-ray-sarong.cyclic.app"
-export const FRONTEND_URL = "http://localhost:3001";//"ecommerce-gules-two.vercel.app"
+export const BACKEND_URL = "https://prickly-ray-sarong.cyclic.app";
+export const FRONTEND_URL = "ecommerce-gules-two.vercel.app";
